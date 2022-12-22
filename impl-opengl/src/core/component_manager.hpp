3dac600b81b17cf4192ab3db7f2a191b701ead97
keechang-choi <rlckd159g@gmail.com>
2022-12-22 19:14:47 +0900

    Add: component manager

---
+++ impl-opengl/src/core/component_manager.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "component_array.hpp"
+#include "types.hpp"
+
+// std
+#include <any>
+#include <memory>
+#include <unordered_map>
+
+namespace ecs_opengl {
+class ComponentManager {
+ public:
+  template <typename T>
+  void RegisterComponent() {
+    const char* type_name = typeid(T).name();
+    assert(component_types.find(type_name) == component_types.end() &&
+           "Component already registered!");
+    component_types.insert({type_name, next_component_type});
+    component_arrays.insert({type_name, std::make_unique<ComponentArray<T>>()});
+
+    next_component_type++;
+  }
+
+  template <typename T>
+  ComponentType GetComponentType() {
+    const char* type_name = typeid(T).name();
+    assert(component_types.find(type_name) != component_types.end() &&
+           "Component not registered!");
+    return component_types[type_name];
+  }
+
+  template <typename T>
+  void RemoveComponent(Entity entity) {
+    GetComponentArray<T>()->RemoveData(entity);
+  }
+
+  template <typename T>
+  T& GetComponent(Entity entity) {
+    return GetComponentArray<T>()->GetDta(entity);
+  }
+
+  void EntityDestroyed(Entity entity) {
+    for (auto const& pair : component_arrays) {
+      auto const& component = pair.second;
+      component->EntityDestroyed(entity);
+    }
+  }
+
+ private:
+  std::unordered_map<const char*, ComponentType> component_types{};
+  std::unordered_map<const char*, std::unique_ptr<IComponentArray>>
+      component_arrays{};
+  ComponentType next_component_type{};
+  template <typename T>
+  std::unique_ptr<ComponentArray<T>>& GetComponentArray() {
+    const char* type_name = typeid(T).name();
+    assert(component_types.find(type_name) != component_types.end() &&
+           "Component not registered!");
+    return component_arrays[type_name];
+  }
+};
+}  // namespace ecs_opengl
